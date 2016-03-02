0b453682385db4c7dc64bfa1aefec12eae9ecf6e
Takuya Aso <milanista224@gmail.com>
2016-03-02 22:12:30 +0900

    Initial Settings

--- DrawCircleLine/Controllers/ViewController.swift
+++ DrawCircleLine/Controllers/ViewController.swift
@@ -10,11 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK:- Life cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
+    // MARK:- Memory Warning
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
